5fd83d3d746c5fd613be1197449f3d3fd48adfc9
Nikolaï Roycourt <nikolai.roycourt@ebuzzing.com>
2015-04-10 16:21:00 +0200

    Demo App update with plugin added

--- demo/Application-demo/platforms/ios/www/cordova_plugins.js
+++ demo/Application-demo/platforms/ios/www/cordova_plugins.js
@@ -1,7 +1,17 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/tv.teads.sdk/www/TeadsPlugin.js",
+        "id": "tv.teads.sdk.TeadsPlugin",
+        "clobbers": [
+            "Teads"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
-{}
+{
+    "tv.teads.sdk": "1.0"
+}
 // BOTTOM OF METADATA
 });
